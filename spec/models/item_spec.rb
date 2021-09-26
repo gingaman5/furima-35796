bd590581f9be0670de44ce3fc6fa0133774ee6f5
gingaman5 <wrath773@icloud.com>
2021-09-26 22:02:47 +0900

    記述修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -32,8 +32,18 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
+      it 'category_idが1では商品出品できない' do
+        @item.category_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
       it 'item_condition_idが空では商品出品できない' do
         @item.item_condition_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item condition can't be blank")
+      end
+      it 'item_condition_idが1では商品出品できない' do
+        @item.item_condition_id = '1'
         @item.valid?
         expect(@item.errors.full_messages).to include("Item condition can't be blank")
       end
@@ -42,13 +52,28 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Ship charge can't be blank")
       end
+      it 'ship_charge_idが1では商品出品できない' do
+        @item.ship_charge_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Ship charge can't be blank")
+      end
       it 'prefecture_idが空では商品出品できない' do
         @item.prefecture_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
+      it 'prefecture_idが1では商品出品できない' do
+        @item.prefecture_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
       it 'ship_day_idが空では商品出品できない' do
         @item.ship_day_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Ship day can't be blank")
+      end
+      it 'ship_day_idが1では商品出品できない' do
+        @item.ship_day_id = '1'
         @item.valid?
         expect(@item.errors.full_messages).to include("Ship day can't be blank")
       end
@@ -58,14 +83,24 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include('Price is not a number')
       end
       it 'priceが¥300~¥9,999,999の間でなければならない' do
-        @item.price = '1~299'
+        @item.price = '299'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price is not a number')
+         expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
+      end
+      it 'priceが¥300~¥9,999,999の間でなければならない' do
+        @item.price = '10,000,000'
+        @item.valid?
+         expect(@item.errors.full_messages).to include('Price is not a number')
       end
       it 'priceが半角数値のみでなければ商品出品できない' do
         @item.price = '２９９'
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+      it 'userが紐付いていないと保存できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
       end
     end
   end
