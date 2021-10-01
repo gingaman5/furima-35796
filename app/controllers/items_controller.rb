91c1305c17d1ddd6e938916aad70380661e5b9d7
gingaman5 <wrath773@icloud.com>
2021-10-01 22:26:53 +0900

    商品情報編集機能完了

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, except: [:index, :create, :new, :show, :update, ]
+  before_action :move_to_index, except:[:index, :create, :new, :show, :update]
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -21,10 +23,33 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:item_name, :item_description, :category_id, :item_condition_id, :ship_charge_id,
                                  :prefecture_id, :ship_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+  def set_item
+  @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    unless current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 end
