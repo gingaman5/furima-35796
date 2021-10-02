5ffb089dfa2455fe5e0abedcd7b03097484281bb
gingaman5 <wrath773@icloud.com>
2021-10-02 15:34:35 +0900

    商品削除機能完了

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, except: [:index, :create, :new, ]
+  before_action :set_item, except: [:index, :create, :new]
   before_action :move_to_index, except: [:index, :create, :new, :show, :update]
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -30,6 +30,15 @@ class ItemsController < ApplicationController
       redirect_to item_path
     else
       render :edit
+    end
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    if @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
     end
   end
 
